11708c247cf746a3c39a26816fa987093630aa9e
Victor hugo paulino <vitinhopaulino60@gmail.com>
2025-08-15 11:09:19 -0300

    [feat] Style of App.js

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/pngegg.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Image source={logo} style={styles.logo} />
+      </View>
+      <Text>Projeto 2B - 15/08/2025</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +21,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+    logo: {
+      height: 128,
+      width: 128,
+    },
 });
