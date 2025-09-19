f2420e6ec5be361ff85650d1efcafc257f051690
Victor hugo paulino <vitinhopaulino60@gmail.com>
2025-09-19 11:09:18 -0300

    atualizacao aula 1909

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { Alert, Image, StyleSheet, Text, TextInput, TouchableOpacity,  View } from 'react-native';
+import {
+   Alert, 
+   Image, 
+   StyleSheet, 
+   Text, 
+   TextInput, 
+   TouchableOpacity,  
+   View 
+  } from 'react-native';
 import logo from './assets/todolist.png';
 import add from './assets/add.png';
 import { useState } from 'react';
+import { FlashList } from '@shopify/flash-list';
+import btnLixo from "./assets/trash-bin.png"
+import Checkbox from 'expo-checkbox';
 
 export default function App() {
   const [tarefa, setTarefa] = useState('')
   const [tarefas, setTarefas] = useState([])
-
 
   const addAdicionar = () => {
     //Alert.alert("TODO list","Valor: " + tarefa);
     setTarefas([tarefa, ...tarefas]);
     setTarefa("")
   };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.viewItemRender}>
+      <Checkbox value={false} />
+      <Text style={styles.textItemRender}>{item}</Text>
+      <TouchableOpacity>
+        <Image source={btnLixo} />
+      </TouchableOpacity>
+    </View>
+    );
+
   return (
     <View style={styles.container}>
       <View style={styles.viewLogo}>
@@ -21,11 +42,23 @@ export default function App() {
         <Text>TODO List</Text>
       </View>
       <View style={styles.viewInput}>
-        <TextInput placeholder='Digite a tarefa' value={tarefa} onChangeText={setTarefa} />
+        <TextInput 
+        placeholder="Digite a tarefa" 
+        value={tarefa} 
+        onChangeText={setTarefa} 
+        />
         <TouchableOpacity onPress={addAdicionar}>
           <Image source={add} style={styles.add} />
         </TouchableOpacity>
       </View>
+      <View style={styles.viewTarefas}>
+        <FlashList
+        data={tarefas}
+        renderItem={renderItem}
+        estimatedItemSize={50}
+        />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -57,5 +90,18 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     marginBottom: 10,
+  },
+  viewTarefas: {
+    width: "100%",
+    flex: 1,
+  },
+  viewItemRender: {
+    height: 75,
+    width: "100%",
+    flexDirection: "row",
+    rowGap: 10,
+  },
+  textItemRender: {
+    flex: 1,
   },
 });
